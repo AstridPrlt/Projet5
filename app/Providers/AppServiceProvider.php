c3009ced8d067ce4818c1154098589f837dcd330
AstridPrlt <AstridPrlt@users.noreply.github.com>
2020-09-23 15:40:22 +0200

    setup #2
    lang fr
    bug sql
    config app.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema; //ajouté pour corriger le bug SQL
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //ajouté pour corriger le bug SQL
     }
 }
